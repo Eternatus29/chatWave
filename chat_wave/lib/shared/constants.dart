c4d4fcf001042fada9001dab3ef2863cc0bd65ea
Eternatus <anik.roy_cs.h21@gla.ac.in>
2023-04-07 03:40:59 +0530

    commit1

---
+++ chat_wave/lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:936904325052:web:614c967df32375bbbe9db9";
+  static String apiKey = "AIzaSyCLzg1FWnYE8r50_nzkKB0X3f6o4xx05rs";
+  static String messagingSenderId = "936904325052";
+  static String projectId = "chatwave-70840";
+  final primaryColor = Color(0xFFee7b64);
+}
